bed713d16158e55f15440c425848a8bfef22ae23
Agusti-salamero <agusti.salamero@est.fib.upc.edu>
2019-03-07 15:20:09 +0100

    Partida v0.1

--- FONT/main/java/Partida.java
+++ FONT/main/java/Partida.java
@@ -1,3 +1,4 @@
+import java.io.Console;
 
 public class Partida {
     private Boolean Guanyador;
@@ -5,5 +6,17 @@ public class Partida {
     private Boolean QuiJuga;
     private Taullel Board;
     private Problema Problem;
-    
+    private Jugador Blanques;
+    private Jugador Negres;
+ 
+    public Partida(Problema P, Jugador b, Jugador n) {
+        this.Problem = P;
+        this.Blanques = b;
+        this.Negres = n;
+        String FEN = "1N1b4/6nr/R5n1/2Ppk2r/K2p2qR/8/2N1PQ2/B6B w - - 0 1"; // falta saber com serà nom de func getFEN
+        int a = FEN.indexOf(" ");
+        System.out.print(aS);
+
+
+    }
 }
