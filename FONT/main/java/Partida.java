fd6d468e260e5f5c6c23b42ab1c1b81652ac2340
Agusti-salamero <agusti.salamero@est.fib.upc.edu>
2019-03-09 11:35:43 +0100

    Partida v0.2

--- FONT/main/java/Partida.java
+++ FONT/main/java/Partida.java
@@ -4,19 +4,42 @@ public class Partida {
     private Boolean Guanyador;
     private Integer Torn;
     private Boolean QuiJuga;
-    private Taullel Board;
-    private Problema Problem;
-    private Jugador Blanques;
-    private Jugador Negres;
+    //  private Taullel Board;
+    //  private Problema Problem;
+    //  private Jugador Blanques;
+    //  private Jugador Negres;
  
-    public Partida(Problema P, Jugador b, Jugador n) {
-        this.Problem = P;
-        this.Blanques = b;
-        this.Negres = n;
+    public Partida(/*Problema P, Jugador b, Jugador n*/) {
+    //    this.Problem = P;
+    //    this.Blanques = b;
+    //    this.Negres = n;
         String FEN = "1N1b4/6nr/R5n1/2Ppk2r/K2p2qR/8/2N1PQ2/B6B w - - 0 1"; // falta saber com serà nom de func getFEN
-        int a = FEN.indexOf(" ");
-        System.out.print(aS);
+        
+        // Decomposition of FEN
+        /* TAULELL PART  */
+        int  endOfBoard = FEN.indexOf(" ");
+        String Taulell_FEN = FEN.substring(0, endOfBoard);
+          System.out.println("BOARD: " + Taulell_FEN);
+        /* A QUI LI TOCA EN AQUEST TORN */
+        char PlayerTurn = FEN.charAt(endOfBoard+1);
+          System.out.println("COLOR PLAYER: " + PlayerTurn);
+        /* HalfMove -> Desde que s'ha matat algu o mogut un peó */
+        int halfmove = Character.getNumericValue(FEN.charAt(endOfBoard+7));
+          System.out.println("Half Move: " + halfmove);       
+        /* FullTurn -> ++ Each time black moves */
+        Torn = Character.getNumericValue(FEN.charAt(endOfBoard+9));
+          System.out.println("TORN: " + Torn);
+
+    }
 
 
+
+
+
+
+
+
+    public static void main(String[] args) {
+        new Partida();
     }
 }
