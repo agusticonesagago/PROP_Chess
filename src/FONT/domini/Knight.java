b5e8c5b72b1c8ea38d518d8a7f34aeb8d3f81a2c
Agustí Conesa <47522077+agusticonesagago@users.noreply.github.com>
2019-03-14 11:44:29 +0100

    Add files via upload

--- src/FONT/domini/Knight.java
+++ src/FONT/domini/Knight.java
@@ -2,6 +2,18 @@ package domini;
 
 import com.sun.org.apache.xpath.internal.operations.Bool;
 
-public class Knight extends Peça {
+  public class Knight extends Peça {
+    int ic = posinicial.getKey();
+    int jc = posinicial.getValue();
+    int ifi = posfinal.getKey();
+    int jfi = posfinal.getValue();
+    boolean blanca = Taulell[ic][jc].getcolor();
 
+    if(caselladins(posfinal) && ((ic-2==ifi && jc-1==jfi)||(ic-1==ifi && jc-2==jfi)||(ic+1==ifi && jc-2==jfi)||
+    (ic+2==ifi && jc-1==jfi)||(ic+2==ifi && jc+1==jfi)||(ic+1==ifi && jc+2==jfi)||(ic-1==ifi && jc+2==jfi)||(ic-2==ifi && jc+1==jfi))){
+      if((Taulell[ifi][jfi].existeix() && Taulell[ifi][jfi].getcolor()!=blanca)||not Taulell[ifi][jfi].existeix() )return true;
+      else return false;
+    }
+    else return false;
+  }
 }
