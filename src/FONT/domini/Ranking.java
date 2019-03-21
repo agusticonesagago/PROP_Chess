ee379ec5efc2cf92ac7d45e588eab37b84fe08a2
EnricSegura1 <enricsegurasanchez@gmail.com>
2019-03-21 09:38:42 +0100

    Ranking v0.1

---
+++ src/FONT/domini/Ranking.java
@@ -0,0 +1,15 @@
+package domini;
+
+public class Ranking {
+    Integer temps;
+    //Jugador
+    //Problema
+
+    public Ranking (Integer t, /*identificador problema, identificador jugador*/) {
+        temps = t;
+    }
+
+    /* GETTERS */
+
+    /* SETTERS */
+}
