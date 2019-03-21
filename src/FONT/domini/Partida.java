2e4cae0f414085e8d4fd304d49a96c2442a0831f
Agusti-salamero <agusti.salamero@est.fib.upc.edu>
2019-03-21 11:51:01 +0100

    Modificació nom Peça -> Peca

--- src/FONT/domini/Partida.java
+++ src/FONT/domini/Partida.java
@@ -1,39 +1,54 @@
 package domini;
 
 
+
+// TODO -> FROM BOARD TO FEN
+
+
 public class Partida {
     private Boolean Guanyador;
     private Integer Torn;
     private Boolean QuiJuga;
     private Taulell Board;
     private Problema Problem;
-    //  private Jugador Blanques;
-    //  private Jugador Negres;
+    private Jugador Blanques;
+    private Jugador Negres;
 
-    public Partida(Problema P /*Jugador b, Jugador n*/) {
+    public Partida(Problema P ,Jugador blanques, Jugador negres) {
         this.Problem = P;
-        //    this.Blanques = b;
-        //    this.Negres = n;
+        this.Blanques = blanques;
+        this.Negres = negres;
 
         // Decomposició de FEN
         String FEN = P.getFEN();
-
-        int endOfBoard = FEN.indexOf(" ");
-        String Taulell_FEN = FEN.substring(0, endOfBoard);
-        Board = new Taulell(Taulell_FEN);
-        char PlayerTurn = FEN.charAt(endOfBoard + 1);
-        if (PlayerTurn == 'w') QuiJuga = true;
-        else QuiJuga = false;
-        Guanyador = null;
-        // int halfmove = Character.getNumericValue(FEN.charAt(endOfBoard + 7));
-        Torn = Character.getNumericValue(FEN.charAt(endOfBoard + 9));
+        if (!FEN.isEmpty()) {
+            int endOfBoard = FEN.indexOf(" ");
+            String Taulell_FEN = FEN.substring(0, endOfBoard);
+            Board = new Taulell(Taulell_FEN);
+            char PlayerTurn = FEN.charAt(endOfBoard + 1);
+            if (PlayerTurn == 'w') QuiJuga = true;
+            else QuiJuga = false;
+            Guanyador = null;
+            // int halfmove = Character.getNumericValue(FEN.charAt(endOfBoard + 7));
+            Torn = Character.getNumericValue(FEN.charAt(endOfBoard + 9));
+        }
     }
 
     public void jugarTorn() {
         if (QuiJuga) {       // Torn Blanques
-
+            System.out.println("TORN BLANQUES");
+            // Blanques moure fitxa
+            // Board actualitzar
+            // ++Torn ja que son les blanques
+            // Check if end of Game
+            QuiJuga = !QuiJuga;// else -> Update QuiJuga
         }
         else {
+            System.out.println("TORN NEGRES");
+            // Negres moure fitxa
+            // Board actualitzar
+            // Cheeck if end of Game
+            QuiJuga = !QuiJuga; // else -> Update QuiJuga
 
         }
     }
--- src/FONT/test/DriverPartida.java
+++ src/FONT/test/DriverPartida.java
@@ -13,16 +13,17 @@ public class DriverPartida {
     }
 
     private static void Tests_2() {
-        Partida t2;
+        Partida t2 = new Partida(new StubProblem("","",""), null, null);
     }
 
     private static void Tests_1() {
-        Partida t1 = new Partida(new StubProblem("","", "1N1b4/6nr/R5n1/2Ppk2r/K2p2qR/8/2N1PQ2/B6B w - - 0 1"));
+        Partida t1 = new Partida(new StubProblem("","", "1N1b4/6nr/R5n1/2Ppk2r/K2p2qR/8/2N1PQ2/B6B w - - 0 1"), null, null);
         System.out.println("Case 1: Hem declarat un taullel amb el format FEN, torn de les blanques i el torn actual es el 1.");
 
         System.out.println("Li toca jugar a Blanques: " + t1.getQuiJuga());
         System.out.println("El torn actual es el: " + t1.getTorn());
         System.out.println("");
+
     }
 
 }
