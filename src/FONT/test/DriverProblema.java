f4c57401f1fcd4c0305d003810eed664cd745584
EnricSegura1 <enricsegurasanchez@gmail.com>
2019-03-28 11:17:17 +0100

    DriverProblema v0.4

--- src/FONT/test/DriverProblema.java
+++ src/FONT/test/DriverProblema.java
@@ -9,11 +9,11 @@ public class DriverProblema {
         String t = "Mat_en_X";
         Problema test = new Problema(t, fen);
 
-        System.out.println("Introdueix un valor valid per a probar les operacions:\n
-                            0: Test de la funcio per Crear Problema\n
-                            1: Test de la funcio cercaProblema\n
-                            2: Test de la funcio eliminarProblema\n
-                            3: Test de la funcio modificarProblema\n");
+        System.out.println("Introdueix un valor valid per a probar les operacions:" + "\n"
+                            + "0: Test de la funcio per Crear Problema" + "\n"
+                            + "1: Test de la funcio cercaProblema" + "\n"
+                            + "2: Test de la funcio eliminarProblema" + "\n"
+                            + "3: Test de la funcio modificarProblema" + "\n");
 
         String fenTest;
         String temaTest;
@@ -25,35 +25,35 @@ public class DriverProblema {
             case 0:
             // 8/8/8/8/8/8/8/NNNkkkRr falla en les normes
             // 5K2/8/8/8/8/8/8/5k2 no es pot fer mat
-             System.out.println("Introdueix el fen a probar\n");
+             System.out.println("Introdueix el fen a probar" + "\n");
              fenTest = sc.nextLine();
-             System.out.println("Introdueix el tema\n");
+             System.out.println("Introdueix el tema" + "\n");
              temaTest = sc.nextLine();
              Problema crear = new Problema(temaTest, fenTest);
             break;
 
             case 1:
-             System.out.println("Introdueix el fen a probar\n");
+             System.out.println("Introdueix el fen a probar" + "\n");
              fenTest = sc.nextLine();
              test.cercaProblema(fenTest);
             break;
 
             case 2:
-             System.out.println("Introdueix el fen a probar\n");
+             System.out.println("Introdueix el fen a probar" + "\n");
              fenTest = sc.nextLine();
              test.eliminarProblema(fenTest);
             break;
 
             case 3:
-             System.out.println("Introdueix el fen a probar\n");
+             System.out.println("Introdueix el fen a probar" + "\n");
              fenTest = sc.nextLine();
-             System.out.println("Introdueix el tema\n");
+             System.out.println("Introdueix el tema" + "\n");
              temaTest = sc.nextLine();
              test.modificarProblema(fenTest, temaTest);
             break;
 
             default:
-            System.out.println("Final del test");
+            System.out.println("Final del test" + "\n");
             break;
         }
     }
