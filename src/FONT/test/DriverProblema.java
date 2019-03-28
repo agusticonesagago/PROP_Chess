21d07028ffa24d5ccda5fdb1a2bbf0237128522f
EnricSegura1 <enricsegurasanchez@gmail.com>
2019-03-28 11:58:06 +0100

    DriverProblema 0.5

--- src/FONT/test/DriverProblema.java
+++ src/FONT/test/DriverProblema.java
@@ -6,11 +6,12 @@ import java.util.Scanner;
 public class DriverProblema {
     public static void main (String[] args) {
         String fen = "1N1b4/6nr/R5n1/2Ppk2r/K2p2qR/8/2N1PQ2/B6B";
-        String t = "Mat_en_X";
+        String t = "Blanques fan mat en 2";
+        System.out.println("Comprobem que funciona la creadora" + "\n");
         Problema test = new Problema(t, fen);
 
         System.out.println("Introdueix un valor valid per a probar les operacions:" + "\n"
-                            + "0: Test de la funcio per Crear Problema" + "\n"
+                            + "0: Test de la funcio per saber Torns i color del que guanya" + "\n"
                             + "1: Test de la funcio cercaProblema" + "\n"
                             + "2: Test de la funcio eliminarProblema" + "\n"
                             + "3: Test de la funcio modificarProblema" + "\n");
@@ -23,13 +24,9 @@ public class DriverProblema {
         cas = sc.nextInt();
         switch (cas){
             case 0:
-            // 8/8/8/8/8/8/8/NNNkkkRr falla en les normes
-            // 5K2/8/8/8/8/8/8/5k2 no es pot fer mat
-             System.out.println("Introdueix el fen a probar" + "\n");
-             fenTest = sc.nextLine();
-             System.out.println("Introdueix el tema" + "\n");
-             temaTest = sc.nextLine();
-             Problema crear = new Problema(temaTest, fenTest);
+             Pair <Integer, Boolean> atributs = test.getTornMat();
+             System.out.println("Per guanyar necessitem:"+ atributs.fisrt() + "\n"
+                                "i el guanyador seria:" + atributs.second() + "\n");
             break;
 
             case 1:
