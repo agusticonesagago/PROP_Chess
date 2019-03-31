133a04580bbdda258542d80d88927324823cc87c
Enric Segura <enricsegurasanchez@gmail.com>
2019-03-31 18:57:08 +0200

    DriverProblema v0.5

--- src/FONT/test/DriverProblema.java
+++ src/FONT/test/DriverProblema.java
@@ -6,54 +6,46 @@ import java.util.Scanner;
 public class DriverProblema {
     public static void main (String[] args) {
         String fen = "1N1b4/6nr/R5n1/2Ppk2r/K2p2qR/8/2N1PQ2/B6B";
-        String t = "Mat_en_X";
+        String t = "Blanques fan mat en 2";
         Problema test = new Problema(t, fen);
 
-        System.out.println("Introdueix un valor valid per a probar les operacions:\n
-                            0: Test de la funcio per Crear Problema\n
-                            1: Test de la funcio cercaProblema\n
-                            2: Test de la funcio eliminarProblema\n
-                            3: Test de la funcio modificarProblema\n");
-
-        String fenTest;
-        String temaTest;
+        System.out.println("Introdueix un valor valid per a probar les operacions:" + "\n"
+                            + "0: Test de la funcio per Crear Problema" + "\n"
+                            + "1: Test de la funcio cercaProblema" + "\n"
+                            + "2: Test de la funcio eliminarProblema" + "\n"
+                            + "3: Test de la funcio modificarProblema" + "\n");
 
         int cas;
         Scanner sc = new Scanner (System.in);
         cas = sc.nextInt();
         switch (cas){
             case 0:
-            // 8/8/8/8/8/8/8/NNNkkkRr falla en les normes
-            // 5K2/8/8/8/8/8/8/5k2 no es pot fer mat
-             System.out.println("Introdueix el fen a probar\n");
-             fenTest = sc.nextLine();
-             System.out.println("Introdueix el tema\n");
-             temaTest = sc.nextLine();
-             Problema crear = new Problema(temaTest, fenTest);
+             System.out.println("Cas 1: el problema conte un FEN que no compleix les normes" + "\n");
+             Problema crear = new Problema("Blanques fan mat en 4", "8/8/8/8/8/8/8/NNNkkkRr");
+
+             System.out.println("Cas 2: el problema conte un FEN que no es pot guanyar en els moviments indicats" + "\n");
+             Problema crear = new Problema("Blanques fan mat en 4", "5K2/8/8/8/8/8/8/5k2");
             break;
 
             case 1:
-             System.out.println("Introdueix el fen a probar\n");
-             fenTest = sc.nextLine();
-             test.cercaProblema(fenTest);
+             System.out.println("Cas 1: el problema no esta en la base de dades" + "\n");
+             test.cercaProblema("8/8/8/8/8/8/8/NNNkkkRr");
+             System.out.println("Cas 2: el problema si esta en la base de dades" + "\n");
+             test.cercaProblema("1N1b4/6nr/R5n1/2Ppk2r/K2p2qR/8/2N1PQ2/B6B");
             break;
 
             case 2:
-             System.out.println("Introdueix el fen a probar\n");
-             fenTest = sc.nextLine();
-             test.eliminarProblema(fenTest);
+             System.out.println("Introdueix el fen a probar" + "\n");
+             test.eliminarProblema("1N1b4/6nr/R5n1/2Ppk2r/K2p2qR/8/2N1PQ2/B6B");
             break;
 
             case 3:
-             System.out.println("Introdueix el fen a probar\n");
-             fenTest = sc.nextLine();
-             System.out.println("Introdueix el tema\n");
-             temaTest = sc.nextLine();
-             test.modificarProblema(fenTest, temaTest);
+             System.out.println("Introdueix el fen a probar" + "\n");
+             test.modificarProblema("1N1b4/6nr/R5n1/2Ppk2r/K2p2qR/8/2N1PQ2/B6B", "Blanques fan mat en 2");
             break;
 
             default:
-            System.out.println("Final del test");
+            System.out.println("Final del test" + "\n");
             break;
         }
     }
