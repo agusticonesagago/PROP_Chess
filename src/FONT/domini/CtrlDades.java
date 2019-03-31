0eb2d4d61ff98479b75316150873ec44fac13b39
Enric Segura <enricsegurasanchez@gmail.com>
2019-03-31 18:56:40 +0200

    CtrlDades v0.1

---
+++ src/FONT/domini/CtrlDades.java
@@ -0,0 +1,31 @@
+package domini;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+public class CtrlDades {
+    Integer identificador;
+
+    public CtrlDades () {
+        identificador = 0;
+    }
+
+    public void add(String f, String d, String t) {
+
+    }
+
+    public Boolean find(String f) {
+        return true;
+    }
+
+    public String giveme(String f){
+        return null;
+    }
+
+    public void destroyProblema(String f){
+
+    }
+
+    public void modifica(String f, String t) {
+
+    }
+}
