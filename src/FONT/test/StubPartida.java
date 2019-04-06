85a79963a6afd1fcd4deb33d281c95acee0cc47b
Enric Segura <enricsegurasanchez@gmail.com>
2019-04-06 18:40:09 +0200

    StubPartida v0.5

--- src/FONT/test/StubPartida.java
+++ src/FONT/test/StubPartida.java
@@ -1,14 +1,20 @@
 package test;
 
 import domini.Partida;
+import domini.Problema;
+import domini.Simple;
 
 public class StubPartida extends Partida {
     public StubPartida() {
-        super(p, null, null);
+        super(new Problema(), null, null);
+    }
+
+    public StubPartida(StubProblem stubProblem, Simple simple, StubJugador stubJugador) {
+        super(stubProblem, simple, stubJugador);
     }
 
     public Boolean simulacorrecte(String fen) {
-        if (f == "1N1b4/6nr/R5n1/2Ppk2r/K2p2qR/8/2N1PQ2/B6B") {
+        if (fen == "1N1b4/6nr/R5n1/2Ppk2r/K2p2qR/8/2N1PQ2/B6B") {
             System.out.println("El problema no es pot solucionar" + "\n");
             return false;
         }
