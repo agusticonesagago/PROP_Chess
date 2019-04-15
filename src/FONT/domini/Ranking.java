53ba25d88c792cd52d7a94982c8ec836d77b56a9
Enric Segura <enricsegurasanchez@gmail.com>
2019-04-15 17:28:33 +0200

    Ranking v1.0

--- src/FONT/domini/Ranking.java
+++ src/FONT/domini/Ranking.java
@@ -2,14 +2,46 @@ package domini;
 
 public class Ranking {
     Integer temps;
-    //Jugador
-    //Problema
+    String jugador;
+    String problema;
 
-    public Ranking (Integer t /*identificador problema, identificador jugador*/) {
+    public Ranking (Integer t, String nomP, String nomJ) {
         temps = t;
+        jugador = nomJ;
+        problema = nomP;
+    }
+
+    public Ranking() {
     }
 
     /* GETTERS */
 
+    public Integer getTemps(){
+        return temps;
+    }
+
+    public String getJugador() {
+        return jugador;
+    }
+
+    public String getProblema() {
+        return problema;
+    }
+
     /* SETTERS */
+
+    public Boolean setTemps(Integer param) {
+        temps = param;
+        return true;
+    }
+
+    public Boolean setJugador(String param) {
+        jugador = param;
+        return true;
+    }
+
+    public Boolean setProblema(String param) {
+        problema = param;
+        return true;
+    }
 }
