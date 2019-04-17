2a4f887ce9bee5e560b83798dec66377c9633c3f
Enric Segura <enricsegurasanchez@gmail.com>
2019-04-17 10:04:06 +0200

    Acabat

--- src/FONT/test/DriverProblema.java
+++ src/FONT/test/DriverProblema.java
@@ -1,20 +1,19 @@
 package test;
 
-import domini.*;
-import java.util.Scanner;
+import domini.Problema;
 import javafx.util.Pair;
 
 public class DriverProblema {
     public static void main (String[] args) {
-        System.out.println("Test de la funcio creadora de problemes" + "\n");
-        String fen = "1N1b4/6nr/R5n1/2Ppk2r/K2p2qR/8/2N1PQ2/B6B";
-        String tema = "Blanques fan mat en 2";
-        String dificultat = "facil";
-        Problema test = new Problema(tema, fen, dificultat, new StubCtrlDades());
+        System.out.println("Test de la classe Problema" + "\n");
+        //Problema a crear
+        String fen1 = "1N1b4/6nr/R5n1/2Ppk2r/K2p2qR/8/2N1PQ2/B6B";
+        String tema1 = "Blanques fan mat en 2";
+        String dificultat1 = "facil";
 
-        System.out.println("Introdueix un valor valid per a probar les operacions:" + "\n"
-                            + "0: Test de la funcio per troba guanyador i moviments Problema" + "\n");
-
+        System.out.println("Test de la creadora de Problema" + "\n");
+        Problema test = new Problema(tema1, fen1, dificultat1, new StubCtrlDades());
+        //Comprobacio de que agafa be les dades
         System.out.println("Test de la funcio que rotorna qui guanyaria i en quans moviments" + "\n");
         Pair <Integer, Boolean> p  = test.getTornMat();
         Integer torn = p.getKey();
